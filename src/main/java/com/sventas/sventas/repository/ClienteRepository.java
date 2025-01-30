fc44fc04794345ab787184a2e1473aa5b8db6692
Javier Cuasapud <edwarcuasapud@gmail.com>
2025-01-29 19:02:17 -0500

    se hizo pruebas rest

---
+++ src/main/java/com/sventas/sventas/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.sventas.sventas.repository;
+import com.sventas.sventas.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+
+}
